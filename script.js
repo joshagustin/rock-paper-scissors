f07a02075bae7392b129d069f13e3e8723f84f89
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-09-28 23:02:09 +0800

    Create function to get player choice

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // get computer choice
-function getComputerChoice () {
+function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     if (number === 0) {
-        return 'rock';
+        return 'Rock';
     }
     else if (number === 1) {
-        return 'scissors';
+        return 'Paper';
     }
-    return 'paper';
+    return 'Scissors';
 }
+
 // get player choice
+function getPlayerChoice() {
+    let choice = '';
+    while (choice !== 'Rock' && choice !== 'Paper' && choice !== 'Scissors') {
+        choice = prompt('Rock, paper, or scissors?');
+        choice = choice.slice(0, 1).toUpperCase() + choice.slice(1).toLowerCase();
+    }
+    return choice;
+}
+
 // play one round
 // play one game of five rounds
